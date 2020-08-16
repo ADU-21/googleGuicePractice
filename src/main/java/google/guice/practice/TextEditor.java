8dca57d0e23d8affac83f385af5d55e972621faa
Luke Du <yidongd@amazon.com>
2020-08-16 16:21:31 -0700

    Logger injection

--- src/main/java/google/guice/practice/TextEditor.java
+++ src/main/java/google/guice/practice/TextEditor.java
@@ -2,17 +2,21 @@ package google.guice.practice;
 
 import javax.inject.Inject;
 import javax.inject.Named;
+import java.util.logging.Logger;
 
 public class TextEditor {
     private final SpellChecker spellChecker;
+    private final Logger logger;
 
     @Inject
-    public TextEditor(SpellChecker spellChecker) {
+    public TextEditor(SpellChecker spellChecker, Logger logger) {
         System.out.println("TextEditor Constructor called");
         this.spellChecker = spellChecker;
+        this.logger = logger;
     }
 
     public void makeSpellCheck() {
+        logger.info("TextEditor.makeSpellCheck called");
         spellChecker.checkSpelling();
     }
 }
